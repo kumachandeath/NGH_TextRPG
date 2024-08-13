616e41c3ccb54d4989b79244810fc1c078925903
kumachandeath <namgungha1@gmail.com>
2024-08-13 12:33:48 +0900

    Create StatusData.cs
    스테이터스 구조체 생성

---
+++ NGH_TextRPG/StatusData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGH_TextRPG
+{
+    public struct StatusData
+    {
+        public int hp;
+        public int attack;
+        public int defense;
+        public float critical;
+        public float evasion;
+    }
+}
