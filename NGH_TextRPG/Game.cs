e380d5ff3863c12d5db4e64bb1c026174e72f261
kumachandeath <namgungha1@gmail.com>
2024-08-13 18:40:57 +0900

    Create Game.cs
    게임 기동용 게임 클래스 생성

---
+++ NGH_TextRPG/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NGH_TextRPG.PlayerFolder;
+using NGH_TextRPG.MonsterFolder;
+using NGH_TextRPG.SceneFolder;
+
+namespace NGH_TextRPG
+{
+    public class Game
+    {
+        private Scene[] scenes;
+        private Scene curScene;
+        public Scene CurScene { get { return curScene; } }
+
+        public static int daysLeft = 100;
+        
+        public Game()
+        {
+            Player player = new Player();
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
