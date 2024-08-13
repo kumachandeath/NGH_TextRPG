8e960994f19d4549b84336002c6bf060feac93b2
kumachandeath <namgungha1@gmail.com>
2024-08-13 17:27:50 +0900

    abstract remove
    클래스 추상화 제거

--- NGH_TextRPG/StatusData.cs
+++ NGH_TextRPG/StatusData.cs
@@ -6,34 +6,17 @@ using System.Threading.Tasks;
 
 namespace NGH_TextRPG
 {
-    public abstract class StatusData
+    public class StatusData
     {
-        protected int curHP;
-        public int CurHP { get { return curHP; } }
-
-        protected int maxHP;
-        public int MaxHp { get { return maxHP; } }
-
-        protected int attack;
-        public int Attack { get { return attack; } }
-
-        protected int defense;
-        public int Defense {  get { return defense; } }
-
-        protected float critical;
-        public float Critical {  get { return critical; } }
-
-        protected float evasion;
-        public float Evasion { get { return evasion; } }
-
-        protected int exp;
-        public int Exp { get { return exp; } }
-
-        protected int level;
-        public int Level { get { return level; } }
-
-        protected int gold;
-        public int Gold { get { return gold; } set { gold = value; } }
+        public int curHP;
+        public int maxHP;
+        public int attack;
+        public int defense;
+        public float critical;
+        public float evasion;
         
+        public int exp;
+        public int level;
+        public int gold;
     }
 }
