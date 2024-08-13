28c1e02a0282aeca80f9f74453251f88c869b384
kumachandeath <namgungha1@gmail.com>
2024-08-13 16:23:56 +0900

    Update Player Initialize
    플레이어 초기화

--- NGH_TextRPG/Player.cs
+++ NGH_TextRPG/Player.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NGH_TextRPG
 {
-    internal class Class1
+    internal class Player
     {
+
+
+        public Player()
+        {
+            StatusData playerStatusData = new StatusData();
+            playerStatusData.hp = 50;
+            playerStatusData.attack = 10;
+            playerStatusData.defense = 10;
+            playerStatusData.critical = 5.0f;
+            playerStatusData.evasion = 5.0f;
+
+            playerStatusData.exp = 0;
+            playerStatusData.level = 1;
+            playerStatusData.gold = 0;
+        }
     }
 }
+
