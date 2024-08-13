32eb53468def4293b2b7a3a0419f5b426fb34e45
kumachandeath <namgungha1@gmail.com>
2024-08-13 15:58:04 +0900

    Update abstract StatusData
    스테이터스 데이터 추상화 업데이트

--- NGH_TextRPG/StatusData.cs
+++ NGH_TextRPG/StatusData.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace NGH_TextRPG
 {
-    public class StatusData
+    public abstract class StatusData
     {
-        public int hp;
-        public int attack;
-        public int defense;
-        public float critical;
-        public float evasion;
+        protected int curHP;
+        public int CurHP { get { return curHP; } }
+
+        protected int maxHP;
+        public int MaxHp { get { return maxHP; } }
+
+        protected int attack;
+        public int Attack { get { return attack; } }
+
+        protected int defense;
+        public int Defense {  get { return defense; } }
+
+        protected float critical;
+        public float Critical {  get { return critical; } }
+
+        protected float evasion;
+        public float Evasion { get { return evasion; } }
+
+        protected int exp;
+        public int Exp { get { return exp; } }
+
+        protected int level;
+        public int Level { get { return level; } }
+
+        protected int gold;
+        public int Gold { get { return gold; } set { gold = value; } }
+        
     }
 }
