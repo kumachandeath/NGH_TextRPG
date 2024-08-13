a3f69dddbd7e3f483152a63ee7fcb90d563ed84a
kumachandeath <namgungha1@gmail.com>
2024-08-13 17:26:12 +0900

    Class Move Folder
    폴더 안으로 클래스 이동

--- NGH_TextRPG/PlayerFolder/Player.cs
+++ NGH_TextRPG/PlayerFolder/Player.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NGH_TextRPG
 {
-    internal class Player
+    public class Player
     {
-
+        StatusData playerStatusData = new StatusData();
 
         public Player()
         {
-            StatusData playerStatusData = new StatusData();
-            playerStatusData.hp = 50;
+            playerStatusData.maxHP = 50;
+            playerStatusData.curHP = playerStatusData.maxHP;
             playerStatusData.attack = 10;
             playerStatusData.defense = 10;
             playerStatusData.critical = 5.0f;
@@ -23,6 +24,11 @@ namespace NGH_TextRPG
             playerStatusData.exp = 0;
             playerStatusData.level = 1;
             playerStatusData.gold = 0;
+        }
+
+        public int LevelUp(int exp)
+        {
+            return playerStatusData.level++;
         }
     }
 }
