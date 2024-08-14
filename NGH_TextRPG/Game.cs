ae4b723b36acbf3f15af7566a7303996b6f61baf
kumachandeath <namgungha1@gmail.com>
2024-08-14 13:05:13 +0900

    Clone Coding
    클론 코딩으로 기틀 구현 참고

--- NGH_TextRPG/Game.cs
+++ NGH_TextRPG/Game.cs
@@ -11,20 +11,76 @@ namespace NGH_TextRPG
 {
     public class Game
     {
+        private bool isRunning;
+
         private Scene[] scenes;
         private Scene curScene;
         public Scene CurScene { get { return curScene; } }
 
         public static int daysLeft = 100;
-        
-        public Game()
-        {
-            Player player = new Player();
-        }
+
+        public Player player = new Player();
 
         public void Run()
         {
-
+            Start();
+            while (isRunning)
+            {
+                Render();
+                Input();
+                Update();
+            }
+            End();
         }
+
+        public void ChangeScene(SceneType sceneType)
+        {
+            curScene.Exit();
+            curScene = scenes[(int)sceneType];
+            curScene.Enter();
+        }
+
+        public void Over()
+        {
+            isRunning = false;
+        }
+
+        private void Start()
+        {
+            isRunning = true;
+
+            scenes = new Scene[(int)SceneType.Size];
+            scenes[(int)SceneType.Title] = new TitleScene(this);
+            scenes[(int)SceneType.Hometown] = new HomeTownScene(this);
+            scenes[(int)SceneType.Shop] = new ShopScene(this);
+            scenes[(int)SceneType.Trainingroom] = new TrainingRoomScene(this);
+            scenes[(int)SceneType.Devilcastle] = new DevilCastleScene(this);
+            scenes[(int)SceneType.Playerinfo] = new PlayerInfoScene(this);
+            scenes[(int)SceneType.Battle] = new BattleScene(this);
+
+            curScene = scenes[(int)SceneType.Title];
+            curScene.Enter();
+        }
+
+        private void End()
+        {
+            curScene.Exit();
+        }
+
+        private void Render()
+        {
+            curScene.Render();
+        }
+
+        private void Input()
+        {
+            curScene.Input();
+        }
+
+        private void Update()
+        {
+            curScene.Update();
+        }
+
     }
 }
--- NGH_TextRPG/SceneFolder/Scene.cs
+++ NGH_TextRPG/SceneFolder/Scene.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace NGH_TextRPG.SceneFolder
 {
-    public class Scene
+    public abstract class Scene
     {
+        protected Game game;
 
+        public Scene(Game game)
+        {
+            this.game = game;
+        }
+
+        public abstract void Enter();
+        public abstract void Input();
+        public abstract void Update();
+        public abstract void Render();
+        public abstract void Exit();
+        
     }
 }
