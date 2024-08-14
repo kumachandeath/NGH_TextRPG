40b7f03c2e48bf4db4339ccde3d4bdb916a115c4
kumachandeath <namgungha1@gmail.com>
2024-08-14 18:03:33 +0900

    Monster fields setting
    몬스터 클래스 필드 세팅

--- NGH_TextRPG/MonsterFolder/Monster.cs
+++ NGH_TextRPG/MonsterFolder/Monster.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NGH_TextRPG.MonsterFolder
 {
-    internal class Monster
+    public class Monster
     {
+        private string name;
+        public int curHP;
+        public int maxHP;
+        public int attack;
+        public int defense;
+        public float critical;
+        public float evasion;
+
+        public int exp;
+        public int level;
+        public int gold;
+
+        public string Name { get; set; }
+
+        public Monster(string name) 
+        {
+            this.name = name;
+            
+        }
     }
 }
