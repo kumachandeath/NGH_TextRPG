926c8add3363fcfad7c5a5df4757c4eb3dd8adb0
kumachandeath <namgungha1@gmail.com>
2024-08-14 17:20:06 +0900

    Complete TitleScene
    타이틀 씬 완성

--- NGH_TextRPG/SceneFolder/TitleScene.cs
+++ NGH_TextRPG/SceneFolder/TitleScene.cs
@@ -8,5 +8,41 @@ namespace NGH_TextRPG.SceneFolder
 {
     internal class TitleScene : Scene
     {
+        public TitleScene(Game game) : base(game) 
+        { 
+        }
+
+        public override void Enter()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+            
+        }
+
+        public override void Input()
+        {
+            Console.ReadKey();
+        }
+
+        public override void Render()
+        {
+            Console.Clear();
+            Console.WriteLine("세계에 위기가 닥쳤다.");
+            Thread.Sleep(2000);
+            Console.WriteLine("마왕이 부활한 것이다.");
+            Thread.Sleep(2000);
+            Console.WriteLine("마왕을 물리치러 여행을 떠나자.");
+            Thread.Sleep(2000);
+            Console.WriteLine("검사의 여행!");
+            Thread.Sleep(2000);
+        }
+
+        public override void Update()
+        {
+            game.ChangeScene(SceneType.Hometown);
+        }
     }
 }
