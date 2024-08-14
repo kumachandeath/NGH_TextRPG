14ed3619ca1dd6fbe2653f237394eb6474844ac2
kumachandeath <namgungha1@gmail.com>
2024-08-14 10:36:57 +0900

    Create HomeTownScene.cs
    새로운 씬 클래스 생성

---
+++ NGH_TextRPG/SceneFolder/HomeTownScene.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGH_TextRPG.SceneFolder
+{
+    internal class Class1
+    {
+    }
+}
