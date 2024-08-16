b20ccb80cec005896a5b26111cbf22361909ab2a
kumachandeath <namgungha1@gmail.com>
2024-08-16 17:49:04 +0900

    Update DevilCastleScene.cs
    마왕성 일부 구현

--- NGH_TextRPG/SceneFolder/DevilCastleScene.cs
+++ NGH_TextRPG/SceneFolder/DevilCastleScene.cs
@@ -8,5 +8,45 @@ namespace NGH_TextRPG.SceneFolder
 {
     internal class DevilCastleScene : Scene
     {
+
+        public DevilCastleScene(Game game) : base(game) 
+        {
+
+        }
+
+        public override void Enter()
+        {
+            Console.Clear();
+            Console.WriteLine("마왕성으로 이동합니다...");
+            Thread.Sleep(2000);
+        }
+
+        public override void Exit()
+        {
+            
+        }
+
+        public override void Input()
+        {
+            Console.ReadLine();
+        }
+
+        public override void Render()
+        {
+            Console.Clear();
+            Console.WriteLine("마왕성에 도착했다. 몇층으로 갈까?");
+            Console.WriteLine("1. 1층(레벨 1~5 추천)");
+            Console.WriteLine("2. 2층(레벨 5~15 추천)");
+            Console.WriteLine("3. 3층(레벨 15~25 추천)");
+            Console.WriteLine("4. 4층(레벨 25~35 추천)");
+            Console.WriteLine("5. 알현실(레벨 40 추천)");
+            Console.WriteLine("0. 마을로 돌아가기(하루가 지나지 않음)");
+            Console.Write("선택 : ");
+        }
+
+        public override void Update()
+        {
+            
+        }
     }
 }
